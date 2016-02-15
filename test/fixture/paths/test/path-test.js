fc7c107faf75d2e1304a47d5513384e6f5c41363
Juha Hytönen <juha.hytonen@haahtela.fi>
2016-02-15 10:33:11 +0200

    Pass 'paths' option to Browserify.

---
+++ test/fixture/paths/test/path-test.js
@@ -0,0 +1,13 @@
+/*global describe, it*/
+'use strict';
+
+var sut = require('paths/some-module');
+
+describe('test', function () {
+
+  it('passes', function () {
+    sut.myMethod();
+    return;
+  });
+
+});
