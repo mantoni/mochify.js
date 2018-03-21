c1b3aa4de0c21d60b1214c51a1e101c6092e341e
Frederik Ring <frederik.ring@gmail.com>
2018-03-21 11:56:24 +0100

    Detect conflicting port values and exit if they don't match

--- test/args-test.js
+++ test/args-test.js
@@ -97,12 +97,29 @@ describe('args', function () {
 
     assert.equal(opts.url.href, 'https://localhost:8080/test.html');
     assert.equal(opts['https-server'], 8080);
+  });
 
-    var withPort = args(
-      ['--url', 'https://localhost:8080/test.html', '--https-server', '4040']
-    );
-    assert.equal(withPort.url.href, 'https://localhost:8080/test.html');
-    assert.equal(withPort['https-server'], 4040);
+  context('with conflicting values given', function () {
+    var nativeExit;
+    var exitCalledWith;
+
+    before(function () {
+      nativeExit = process.exit;
+      process.exit = function () {
+        exitCalledWith = [].slice.call(arguments);
+      };
+    });
+
+    after(function () {
+      process.exit = nativeExit;
+    });
+
+    it('logs and exits with status code 1', function () {
+      args(
+        ['--url', 'https://localhost:8080/test.html', '--https-server', '4040']
+      );
+      assert.deepStrictEqual(exitCalledWith, [1]);
+    });
   });
 
   it('parses --wd-file', function () {
@@ -283,6 +300,7 @@ describe('args', function () {
     var noValueOpts = args(['--https-server']);
     assert.strictEqual(noValueOpts['https-server'], 0);
   });
+
 
   it('parses --allow-chrome-as-root', function () {
     var opts = args(['--allow-chrome-as-root']);
