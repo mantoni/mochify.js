354f7f3a1cd58a422d43c7d772e1b6f75882c1f8
Maximilian Antoni <mail@maxantoni.de>
2021-07-04 22:54:12 +0200

    Use yargs and glob

--- cli/index.js
+++ cli/index.js
@@ -1,26 +1,36 @@
 #!/usr/bin/env node
 'use strict';
 
+const { promisify } = require('util');
+const glob = promisify(require('glob'));
+const yargs = require('yargs/yargs');
+const { hideBin } = require('yargs/helpers');
 const { mochify } = require('@mochify/mochify');
 
-const argv = process.argv.slice(2);
-
-let reporter = 'spec';
-if (argv[0] === '-R' || argv[0] === '--reporter') {
-  reporter = argv[1];
-}
+const opts = yargs(hideBin(process.argv))
+  .option('reporter', {
+    alias: 'R',
+    type: 'string'
+  })
+  .parse();
 
 // The bundle command to run (from config):
-const bundle = 'browserify --debug';
-
-// Resolved files from glob expression:
-const files = ['./test.js'];
+const config = {
+  bundle: 'browserify --debug',
+  reporter: opts.reporter
+};
 
 (async () => {
+  config.files = await resolveFiles(opts._);
   try {
-    await mochify({ reporter, bundle, files });
+    await mochify(config);
   } catch (e) {
     console.error(e.stack);
     process.exitCode = 1;
   }
 })();
+
+async function resolveFiles(patterns) {
+  const matches = await Promise.all(patterns.map((pattern) => glob(pattern)));
+  return matches.reduce((all, match) => all.concat(match), []);
+}
