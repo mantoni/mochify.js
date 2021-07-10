fab3f23fe2c8d59397cae0ffd7c0885c08383d04
Frederik Ring <frederik.ring@gmail.com>
2021-07-10 12:39:46 +0200

    Use non-blocking loop to poll for events

--- mochify/lib/poll-events.js
+++ mochify/lib/poll-events.js
@@ -3,22 +3,28 @@
 exports.pollEvents = pollEvents;
 
 async function pollEvents(driver, emit) {
-  for (;;) {
-    const events = await driver.evaluateReturn('mocha.mochify_pollEvents()');
-    if (!events) {
-      continue;
-    }
-    for (const [event, data] of events) {
-      if (event === 'mochify.callback') {
-        return data.code || 0; // stop polling
+  let interval;
+  const result = await new Promise((resolve) => {
+    interval = setInterval(async () => {
+      const events = await driver.evaluateReturn('mocha.mochify_pollEvents()');
+      if (!events) {
+        return;
       }
-      if (event === 'mochify.coverage') {
-        global.__coverage__ = data;
-      } else if (event.startsWith('console.')) {
-        console[event.substring(8)](...data);
-      } else {
-        emit(event, data);
+      for (const [event, data] of events) {
+        if (event === 'mochify.callback') {
+          resolve(data.code || 0);
+          return; // stop polling
+        }
+        if (event === 'mochify.coverage') {
+          global.__coverage__ = data;
+        } else if (event.startsWith('console.')) {
+          console[event.substring(8)](...data);
+        } else {
+          emit(event, data);
+        }
       }
-    }
-  }
+    }, 1);
+  });
+  clearInterval(interval);
+  return result;
 }
