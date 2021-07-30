dc1f3152061aeb5b054d45f2f08a2592b3688013
Maximilian Antoni <mail@maxantoni.de>
2021-07-30 17:45:40 +0200

    Load config with cosmiconfig

--- cli/index.js
+++ cli/index.js
@@ -8,6 +8,9 @@ const { hideBin } = require('yargs/helpers');
 const { mochify } = require('@mochify/mochify');
 
 const opts = yargs(hideBin(process.argv))
+  .option('config', {
+    type: 'string'
+  })
   .option('driver', {
     type: 'string'
   })
@@ -29,20 +32,18 @@ const opts = yargs(hideBin(process.argv))
   })
   .parse();
 
-// The bundle command to run (from config):
-const config = {
-  driver: opts.driver,
-  driver_options: opts['driver-option'],
-  bundle: opts.bundle,
-  reporter: opts.reporter,
-  serve: opts.serve,
-  server_options: opts['server-option']
-};
+if (opts['driver-option']) {
+  opts.driver_options = opts['driver-option'];
+}
+if (opts['server-option']) {
+  opts.server_options = opts['server-option'];
+}
 
 (async () => {
-  config.files = await resolveFiles(opts._);
+  const files = await resolveFiles(opts._);
+  const options = Object.assign({ files }, opts);
   try {
-    await mochify(config);
+    await mochify(options);
   } catch (e) {
     console.error(e.stack);
     process.exitCode = 1;
