cbed9f5313e252d6e108aa5bf40e3878ed575240
Maximilian Antoni <mail@maxantoni.de>
2021-08-01 15:59:23 +0200

    Add config integration tests

---
+++ cli/test/config.integration.js
@@ -0,0 +1,41 @@
+'use strict';
+
+const path = require('path');
+const { assert } = require('@sinonjs/referee-sinon');
+const execa = require('execa');
+
+describe('config', () => {
+  it('mochify.config.js', async () => {
+    const result = await execa('../../index.js', ['passes.js'], {
+      cwd: path.join(__dirname, 'fixture')
+    });
+
+    const json = JSON.parse(result.stdout);
+    assert.equals(json.tests.length, 1);
+    assert.equals(json.tests[0].fullTitle, 'test passes');
+  });
+
+  it('custom.config.yaml', async () => {
+    const result = await execa(
+      '../../index.js',
+      ['--config', 'custom.config.yaml', 'passes.js'],
+      {
+        cwd: path.join(__dirname, 'fixture')
+      }
+    );
+
+    assert.match(result.stdout, 'ok 1 test passes');
+  });
+
+  it('overrides config with command line option', async () => {
+    const result = await execa(
+      '../../index.js',
+      ['--reporter', 'tap', 'passes.js'],
+      {
+        cwd: path.join(__dirname, 'fixture')
+      }
+    );
+
+    assert.match(result.stdout, 'ok 1 test passes');
+  });
+});
