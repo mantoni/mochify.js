c3583429577a8335b3cd19cfab3031e5117d08f4
Maximilian Antoni <mail@maxantoni.de>
2021-08-01 13:26:14 +0200

    Add tests for inject-script

---
+++ mochify/lib/inject-script.test.js
@@ -0,0 +1,48 @@
+'use strict';
+
+const { assert, sinon } = require('@sinonjs/referee-sinon');
+const { injectScript } = require('./inject-script');
+
+describe('mochify/lib/inject-script', () => {
+  const MAX_SCRIPT_CHUNK = 700 * 1000;
+
+  it('evaluates mochify_receive with given script', () => {
+    const driver = { evaluate: sinon.fake.returns(sinon.promise()) };
+
+    injectScript(driver, 'console.log("Hi!")');
+
+    assert.calledOnceWith(
+      driver.evaluate,
+      'mocha.mochify_receive("console.log(\\"Hi!\\")")'
+    );
+  });
+
+  it('evaluates mochify_run', async () => {
+    const driver = { evaluate: sinon.fake.resolves() };
+
+    const promise = injectScript(driver, 'console.log("Hi!")');
+
+    await assert.resolves(promise);
+    assert.calledTwice(driver.evaluate);
+    assert.calledWith(driver.evaluate, 'mocha.mochify_run()');
+  });
+
+  it('splits script into chunks and invokes mochify_receive twice', async () => {
+    const driver = { evaluate: sinon.fake.resolves() };
+    const script = {
+      length: MAX_SCRIPT_CHUNK + 1,
+      substring: sinon.fake((from) => (from === 0 ? 'first' : 'second'))
+    };
+
+    const promise = injectScript(driver, script);
+
+    await assert.resolves(promise);
+    assert.calledTwice(script.substring);
+    assert.calledWith(script.substring, 0, MAX_SCRIPT_CHUNK);
+    assert.calledWith(script.substring, MAX_SCRIPT_CHUNK);
+    assert.calledThrice(driver.evaluate);
+    assert.calledWith(driver.evaluate, 'mocha.mochify_receive("first")');
+    assert.calledWith(driver.evaluate, 'mocha.mochify_receive("second")');
+    assert.calledWith(driver.evaluate, 'mocha.mochify_run()');
+  });
+});
