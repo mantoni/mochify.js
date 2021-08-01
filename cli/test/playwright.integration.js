8a3e1e23af6b7751057ead505c93922a3551f4cd
Maximilian Antoni <mail@maxantoni.de>
2021-08-01 16:15:36 +0200

    Add cli playwright integration tests

---
+++ cli/test/playwright.integration.js
@@ -0,0 +1,42 @@
+'use strict';
+
+const path = require('path');
+const { assert } = require('@sinonjs/referee-sinon');
+const execa = require('execa');
+
+describe('playwright', () => {
+  it('passes', async () => {
+    const result = await execa(
+      '../../index.js',
+      ['--driver', 'playwright', 'passes.js'],
+      {
+        cwd: path.join(__dirname, 'fixture')
+      }
+    );
+
+    assert.isFalse(result.failed);
+    const json = JSON.parse(result.stdout);
+    assert.equals(json.tests.length, 1);
+    assert.equals(json.tests[0].fullTitle, 'test passes');
+  });
+
+  it('fails', async () => {
+    let result;
+    try {
+      result = await execa(
+        '../../index.js',
+        ['--driver', 'playwright', 'fails.js'],
+        {
+          cwd: path.join(__dirname, 'fixture')
+        }
+      );
+    } catch (error) {
+      result = error;
+    }
+
+    assert.isTrue(result.failed);
+    const json = JSON.parse(result.stdout);
+    assert.equals(json.tests.length, 1);
+    assert.equals(json.tests[0].fullTitle, 'test fails');
+  });
+});
