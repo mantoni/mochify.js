232ec639116bd606b62adce8fce23dae1119c19a
Maximilian Antoni <mail@maxantoni.de>
2021-09-05 21:15:46 +0200

    Convert client.js to es5 to support old browsers

--- mochify/client.js
+++ mochify/client.js
@@ -1,20 +1,11 @@
 /*globals Mocha, mocha, window, document*/
+/*eslint-disable no-var, prefer-arrow-callback, mocha/prefer-arrow-callback,
+  object-shorthand, prefer-template*/
 'use strict';
 
-const {
-  EVENT_RUN_BEGIN,
-  EVENT_RUN_END,
-  EVENT_TEST_FAIL,
-  EVENT_TEST_PASS,
-  EVENT_TEST_PENDING,
-  EVENT_TEST_END,
-  EVENT_SUITE_BEGIN,
-  EVENT_SUITE_END,
-  EVENT_DELAY_BEGIN,
-  EVENT_DELAY_END
-} = Mocha.Runner.constants;
+var constants = Mocha.Runner.constants;
 
-const error_keys = [
+var error_keys = [
   'name',
   'message',
   'stack',
@@ -23,7 +14,7 @@ const error_keys = [
   'actual',
   'operator'
 ];
-const test_keys = [
+var test_keys = [
   'title',
   'type',
   'state',
@@ -35,24 +26,24 @@ const test_keys = [
   '_currentRetry'
 ];
 
-const { hasOwnProperty } = Object.prototype;
-const { slice } = Array.prototype;
+var hasOwnProperty = Object.prototype.hasOwnProperty;
+var slice = Array.prototype.slice;
 
 function copy(keys, from, to) {
-  for (const key of keys) {
+  keys.forEach(function (key) {
     if (hasOwnProperty.call(from, key)) {
       to[key] = from[key];
     }
-  }
+  });
 }
 
-const queue = [];
+var queue = [];
 
 function pollEvents() {
   if (!queue.length) {
     return null;
   }
-  const events = queue.slice();
+  var events = queue.slice();
   queue.length = 0;
   return events;
 }
@@ -62,7 +53,7 @@ function write(event, data) {
 }
 
 function getTestData(test) {
-  const json = {
+  var json = {
     _fullTitle: test.fullTitle(),
     _titlePath: test.titlePath()
   };
@@ -71,45 +62,55 @@ function getTestData(test) {
 }
 
 function forward(runner, event, processor) {
-  runner.on(event, (object, err) => {
+  runner.on(event, function (object, err) {
     write(event, processor(object, err));
   });
 }
 
-class MochifyReporter {
-  constructor(runner) {
-    const { stats } = runner;
+function MochifyReporter(runner) {
+  var stats = runner.stats;
 
-    forward(runner, EVENT_RUN_BEGIN, () => ({
+  forward(runner, constants.EVENT_RUN_BEGIN, function () {
+    return {
       start: stats.start.toISOString()
-    }));
+    };
+  });
 
-    forward(runner, EVENT_RUN_END, () => ({
+  forward(runner, constants.EVENT_RUN_END, function () {
+    return {
       end: stats.end.toISOString(),
       duration: stats.duration
-    }));
+    };
+  });
 
-    forward(runner, EVENT_SUITE_BEGIN, (suite) => ({
+  forward(runner, constants.EVENT_SUITE_BEGIN, function (suite) {
+    return {
       root: suite.root,
       title: suite.title,
       pending: suite.pending,
       delayed: suite.delayed
-    }));
+    };
+  });
 
-    forward(runner, EVENT_SUITE_END, () => ({}));
-    forward(runner, EVENT_DELAY_BEGIN, () => ({}));
-    forward(runner, EVENT_DELAY_END, () => ({}));
+  forward(runner, constants.EVENT_SUITE_END, function () {
+    return {};
+  });
+  forward(runner, constants.EVENT_DELAY_BEGIN, function () {
+    return {};
+  });
+  forward(runner, constants.EVENT_DELAY_END, function () {
+    return {};
+  });
 
-    forward(runner, EVENT_TEST_PASS, getTestData);
-    forward(runner, EVENT_TEST_PENDING, getTestData);
-    forward(runner, EVENT_TEST_FAIL, (test, err) => {
-      const json = getTestData(test);
-      json.err = {};
-      copy(error_keys, err, json.err);
-      return json;
-    });
-    forward(runner, EVENT_TEST_END, getTestData);
-  }
+  forward(runner, constants.EVENT_TEST_PASS, getTestData);
+  forward(runner, constants.EVENT_TEST_PENDING, getTestData);
+  forward(runner, constants.EVENT_TEST_FAIL, function (test, err) {
+    var json = getTestData(test);
+    json.err = {};
+    copy(error_keys, err, json.err);
+    return json;
+  });
+  forward(runner, constants.EVENT_TEST_END, getTestData);
 }
 
 mocha.reporter(MochifyReporter);
@@ -117,37 +118,37 @@ mocha.setup(/* MOCHIFY_UI */);
 mocha.timeout(/* MOCHIFY_TIMEOUT */);
 mocha.mochify_pollEvents = pollEvents;
 
-const chunks = [];
-mocha.mochify_receive = (chunk) => {
+var chunks = [];
+mocha.mochify_receive = function (chunk) {
   chunks.push(chunk);
 };
-mocha.mochify_run = () => {
+mocha.mochify_run = function () {
   // Inject script
-  const s = document.createElement('script');
+  var s = document.createElement('script');
   s.type = 'text/javascript';
   s.textContent = chunks.join('');
   document.body.appendChild(s);
   // Run mocha
-  mocha.run((code) => {
+  mocha.run(function (code) {
     if (typeof __coverage__ !== 'undefined') {
       write('mochify.coverage', window.__coverage__);
     }
-    write('mochify.callback', { code });
+    write('mochify.callback', { code: code });
   });
 };
 
-['debug', 'log', 'info', 'warn', 'error'].forEach((name) => {
+['debug', 'log', 'info', 'warn', 'error'].forEach(function (name) {
   if (console[name]) {
     console[name] = function () {
-      write(`console.${name}`, slice.call(arguments));
+      write('console.' + name, slice.call(arguments));
     };
   }
 });
 
-window.onerror = (msg, file, line, column, err) => {
+window.onerror = function (msg, file, line, column, err) {
   if (err) {
     console.error(err.stack);
   } else {
-    console.error(`${msg}\n    at ${file}:${line}:${column}`);
+    console.error(msg + '\n    at ' + file + ':' + line + ':' + column);
   }
 };
